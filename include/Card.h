adb61604a81247aeda29736176e4893f4ace9794
Sasy00 <super.salvatore00@gmail.com>
2021-07-26 20:15:43 +0200

    fix: typo

--- include/Card.h
+++ include/Card.h
@@ -8,9 +8,9 @@ public:
     Card(int suit, int rank);
 
     // 0 <= suit <= 3
-    int getSuit() const { return id / 10; }
+    int getSuit() const { return _id / 10; }
     // 1 <= rank <= 10
-    int getRank() const { return (id % 10) + 1; }
+    int getRank() const { return (_id % 10) + 1; }
 
 private:
     // 0 <= _id <= 39
