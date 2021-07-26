bada051e88dcc63bf6ebbecd044e00909378ff64
Sasy00 <super.salvatore00@gmail.com>
2021-07-26 19:29:09 +0200

    feat: add class Card

--- include/Card.h
+++ include/Card.h
@@ -1,4 +1,20 @@
 #ifndef CARD_H
 #define CARD_H
 
+class Card
+{
+public:
+    Card(int id);
+    Card(int suit, int rank);
+
+    // 0 <= suit <= 3
+    int getSuit() const { return id / 10; }
+    // 1 <= rank <= 10
+    int getRank() const { return (id % 10) + 1; }
+
+private:
+    // 0 <= _id <= 39
+    const int _id;
+};
+
 #endif
