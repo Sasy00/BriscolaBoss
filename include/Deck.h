e623568d1e121d0bbc6decd84b04d93caf502d59
Sasy00 <super.salvatore00@gmail.com>
2021-07-28 16:47:11 +0200

    comment: added comments

--- include/Deck.h
+++ include/Deck.h
@@ -8,8 +8,19 @@ class Deck
 {
 public:
     Deck();
+    /**
+     * @brief resets the deck removing all old cards and recreating the entire deck
+     */ 
     void reset();
+
+    /**
+     * @brief shuffles the deck
+     */ 
     void shuffle();
+    
+    /**
+     * @brief removes a card from the deck and returns a copy of that card
+     */ 
     Card draw();
 private:
     std::vector<Card> _cards;
