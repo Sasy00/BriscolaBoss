787cf6f71be5ee76d1daf4c7fecff947ddcc1946
Sasy00 <super.salvatore00@gmail.com>
2021-07-28 19:59:57 +0200

    feat: implemented GameManager::draw()

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -1,27 +1,71 @@
 #include "GameManager.h"
 #include <cmath>
 #include <ctime>
+#include <iostream>
 GameManager::GameManager(int nPlayers) : _nPlayers(nPlayers),
                                          _briscola(nullptr)
 {
-
 }
-
+GameManager::~GameManager()
+{
+    delete _briscola;
+}
 void GameManager::init()
 {
     srand(time(0));
+    
     _deck.reset();
     _deck.shuffle();
+    
     _briscola = new Card(_deck.draw());
+    
     _hands.clear();
-    _collected.clear();
-    _playedCards.clear();
-    for(int i = 0; i < 3; ++i)
+    for(int i = 0; i < _nPlayers; ++i)
     {
-        for(int j = 0; j < _nPlayers; ++j)
+        _hands.push_back(std::vector<Card>());
+    }
+    
+    _collected.clear();
+    for(int i = 0; i < _nPlayers; ++i)
+    {
+        _collected.push_back(std::vector<Card>());
+    }
+    
+    _playedCards.clear();
+    
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < _nPlayers; ++j)
         {
             _hands[j].push_back(_deck.draw());
         }
     }
     _currentPlayer = rand() % _nPlayers;
+}
+
+void GameManager::update()
+{
+    draw();
+    //getInput();
+}
+
+void GameManager::draw() const
+{
+    std::cout << "Cards in deck: " << _deck.size() << std::endl;
+    for (int i = 0; i < _nPlayers; ++i)
+    {
+        std::cout << "Card in player " << i << "hand: ";
+        for (auto it = _hands[i].begin(); it != _hands[i].end(); ++it)
+        {
+            std::cout << *it << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Played Cards: ";
+    for (auto it = _playedCards.begin(); it != _playedCards.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
 }
