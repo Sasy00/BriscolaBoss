b0321a8e6ce54eb6d95562b43b736f8e140ec646
Sasy00 <super.salvatore00@gmail.com>
2021-07-28 16:31:36 +0200

    feat: implemented Deck::shuffle

--- src/Deck.cpp
+++ src/Deck.cpp
@@ -1,15 +1,26 @@
 #include "Deck.h"
-
+#include <cmath>
+#include <ctime>
+#include <algorithm>
 Deck::Deck()
 {
-
 }
 
 void Deck::reset()
 {
     _cards.clear();
-    for(int i = 0; i < 40; ++i)
+    for (int i = 0; i < 40; ++i)
     {
         _cards.push_back(Card(i));
+    }
+}
+
+void Deck::shuffle()
+{
+    srand(time(0));
+    for (int i = _cards.size() - 1; i > 0; ++i)
+    {
+        int index = rand() % (i + 1);
+        std::iter_swap(_cards.begin() + index, _cards.begin() + i);
     }
 }
