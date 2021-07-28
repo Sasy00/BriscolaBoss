09c001d333f2ef77e7151aa1160e513fa84b14cf
Sasy00 <super.salvatore00@gmail.com>
2021-07-28 19:43:38 +0200

    feat: added Deck::size()

--- include/Deck.h
+++ include/Deck.h
@@ -10,18 +10,21 @@ public:
     Deck();
     /**
      * @brief resets the deck removing all old cards and recreating the entire deck
-     */ 
+     */
     void reset();
 
     /**
      * @brief shuffles the deck
-     */ 
+     */
     void shuffle();
-    
+
     /**
      * @brief removes a card from the deck and returns a copy of that card
-     */ 
+     */
     Card draw();
+
+    inline int size() const { return _cards.size(); }
+
 private:
     std::vector<Card> _cards;
 };
