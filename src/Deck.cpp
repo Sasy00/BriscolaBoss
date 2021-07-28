e1a94a69fa714213b0081959802d2ccf65937795
Sasy00 <super.salvatore00@gmail.com>
2021-07-28 16:13:03 +0200

    fix: changed pointers to objects

--- src/Deck.cpp
+++ src/Deck.cpp
@@ -7,13 +7,9 @@ Deck::Deck()
 
 void Deck::reset()
 {
-    for(auto it = _cards.begin(); it != _cards.end(); ++it)
-    {
-        delete *it;
-    }
     _cards.clear();
     for(int i = 0; i < 40; ++i)
     {
-        _cards.push_back(new Card(i));
+        _cards.push_back(Card(i));
     }
 }
